c129dedef79848d0fa8de642e73afa151c63b507
Myti <2132799848@qq.com>
2021-11-15 09:34:48 +0900

    test

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<template>
+
+
+  <v-app light>
+
+  <!--The SideMenu Component go here-->
+  <SideMenu :drawer="drawer"  :api_key="api_key" @selectsource="setResource" ></SideMenu>
+
+  <v-toolbar fixed app light clipped-left color="primary" class="elevation-2">
+    <v-toolbar-side-icon @click="drawer = !drawer"  class="white--text"></v-toolbar-side-icon>
+    <v-toolbar-title class="white--text">News App</v-toolbar-title>
+  </v-toolbar>
+
+  <v-content>
+    <v-container fluid>
+
+      <!--The MainContent Component go here-->
+      <MainContent :articles="articles"></MainContent>
+
+    </v-container>
+   </v-content>
+   <v-footer class="secondary" app>
+      <v-layout row wrap align-center>
+        <v-flex xs12>
+          <div class="white--text ml-3">
+            Made with
+            <v-icon class="red--text">favorite</v-icon>
+            by <a class="white--text" href="https://vuetifyjs.com" target="_blank">Vuetify</a>
+            and <a class="white--text" href="https://github.com/mytiii" target="_blank">mytiii</a>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-footer>
+  </v-app>
+
+
+</template>
+
+<script>
+
+import axios from 'axios' //importing the axios a HTTP library to connects the app with the API
+import SideMenu from './components/SideMenu.vue' // import the SideMenu component
+import MainContent from './components/MainContent.vue' // import the MainContent component
+
+
+export default {
+
+  components: {
+      SideMenu,
+      MainContent
+  },
+
+  //返回数据
+  data() {
+    return {
+      drawer: true, //true/false表示开关
+      api_key:'c2c643c4011646c98038c89c237f5be2',//api接口密钥
+      articles: [],
+      errors: []
+    }
+  },
+
+  //创建使用api接口网址
+  created () {
+    axios.get('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey='+this.api_key)
+      .then(response => {
+        this.articles = response.data.articles
+        console.log('data:')
+        console.log(response.data.articles)
+      .catch(e => {
+        this.errors.push(e)
+      })
+    })
+  },
+
+
+  methods: {
+
+      setResource(source){
+
+        axios.get('https://newsapi.org/v2/top-headlines?sources='+source+'&apiKey='+this.api_key)
+        .then(response => {
+          //this.articles = response.data.articles
+          this.articles = response.data.articles
+          console.log('Source Articles:')
+          console.log(response.data.articles) // This will give you access to the full object
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+
+      }
+
+
+   }
+
+  }
+</script>
